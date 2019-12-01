a569c9ad569b0b9bef7f4e6424602c8300a2dc77
ngsengleng <48010218+ngsengleng@users.noreply.github.com>
2019-12-01 17:38:02 +0800

    aoc1.py

--- AoC1/AoC1.py
+++ AoC1/AoC1.py
@@ -5,21 +5,19 @@ with open(r"AoC1.txt") as f:
     lines = f.readlines()
     lines[:] = [line.strip("\n") for line in lines]
     lines[:] = [int(line) for line in lines]
-    #print(lines)
 
 #part1
 sum = 0
 for line in lines:
     out = math.floor(int(line)/3)-2
     sum = sum + out
-#print(sum)
+print(sum)
 
 #part2
 sum = 0
 for line in lines:
     while line > 0:
         out = math.floor(int(line)/3)-2
-        #print(out)
         if out > 0:
             sum = sum + out
         line = out
