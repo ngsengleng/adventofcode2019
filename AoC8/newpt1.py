9f398a8213f4e9e2bb4f507da33d6558b00e6e95
ngsengleng <48010218+ngsengleng@users.noreply.github.com>
2019-12-08 16:18:15 +0800

    learning use of reduce(not original soln)

--- AoC8/newpt1.py
+++ AoC8/newpt1.py
@@ -7,4 +7,7 @@ height = 6
 size = width*height
 imageFile = [image[i:i+size] for i in range(0,len(image),size)]
 zeroes = reduce(lambda a,b: a if a.count("0") < b.count("0") else b,imageFile)
+#reduce takes in the first two iterables from imageFile,passes into lambda
+#it then passes into lambda the result of previous lambda call and next iterable of imageFile
+#cycle continues until imageFile is exhausted
 print(zeroes.count("1")*zeroes.count("2"))
